c33fdc4ff61d76e143617e9d5e18fa55cfa4b417
ayu24 <alany1510@gmail.com>
2018-12-02 15:40:44 -0600

    attempt to make Play button work

--- app/src/main/java/com/example/bob60/booleanlogicgame/MainActivity.java
+++ app/src/main/java/com/example/bob60/booleanlogicgame/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.bob60.booleanlogicgame;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonPressPlay = findViewById(R.id.button_play);
+        buttonPressPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startGame();
+            }
+        });
     }
+
+    private void startGame() {
+        Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+        startActivity(intent);
+    }
+
+
 
 }
