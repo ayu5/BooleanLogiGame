49103b068bc16d48e11e9d85f165e03ed2c169fd
ayu5 <alany1510@gmail.com>
2018-12-07 11:29:42 -0600

    minor adjustments to color and made working practice mode

---
+++ app/src/main/java/com/example/bob60/booleanlogicgame/P1testActivity.java
@@ -0,0 +1,13 @@
+package com.example.bob60.booleanlogicgame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class P1testActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_p1test);
+    }
+}
