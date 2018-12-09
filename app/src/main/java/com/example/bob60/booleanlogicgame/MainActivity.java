2e89d2fc7ebc2e02475a3d8c9ee5290e4fc88273
apieri2 <apieri2@illinois.edu>
2018-12-08 18:30:44 -0600

    Fixed score and added reset button (location to be fixed)

--- app/src/main/java/com/example/bob60/booleanlogicgame/MainActivity.java
+++ app/src/main/java/com/example/bob60/booleanlogicgame/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.SharedPreferences;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -21,6 +22,13 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i("on create", "on create");
+
+        SharedPreferences prefs = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt(PtestActivity.MY_SCORE, 0);
+        editor.apply();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -40,6 +48,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startPractice();
+            }
+        });
+
+        Button buttonResetScore = findViewById(R.id.button_resetScore);
+        buttonResetScore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetHighScore();
             }
         });
 
@@ -65,30 +81,53 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        SharedPreferences prefs = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+
+        Log.i("on activity", "on activity");
+
         super.onActivityResult(requestCode, resultCode, data);
 
         if (requestCode == REQUEST_CODE_QUIZ) {
-            if (resultCode == RESULT_OK) {
-                int score = data.getIntExtra(PtestActivity.MY_SCORE, 0);
-                if (score > highscore) {
-                    updateHighScore(score);
-                }
+            int score = prefs.getInt(PtestActivity.MY_SCORE, 0);
+            Log.i("score", "score: " + score);
+            Log.i("high score", "high score: " + highscore);
+            if (score > highscore) {
+                updateHighScore(score);
+                Log.i("update", "updated");
             }
         }
+        loadHighScore();
+
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt(PtestActivity.MY_SCORE, 0);
+        editor.apply();
     }
 
     private void loadHighScore() {
         SharedPreferences prefs = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
         highscore = prefs.getInt(KEY_HIGHSCORE, 0);
-        textViewHighScore.setText("Highscore: " + highscore);
+        textViewHighScore.setText("High Score: " + highscore);
+        Log.i("loaded", "loaded: " + highscore);
     }
-    private void updateHighScore(int highscoreNew) {
-        highscore = highscoreNew;
-        textViewHighScore.setText("Highscore: " + highscore);
+    private void updateHighScore(int highScoreNew) {
+        highscore = highScoreNew;
+
+        Log.i("updated", "updated: " + highscore);
 
         SharedPreferences prefs = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
         SharedPreferences.Editor editor = prefs.edit();
         editor.putInt(KEY_HIGHSCORE, highscore);
+        editor.apply();
+    }
+    private void resetHighScore() {
+        highscore = 0;
+
+        textViewHighScore = findViewById(R.id.text_view_highscore);
+        textViewHighScore.setText("Highscore: 0");
+
+        SharedPreferences prefs = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt(KEY_HIGHSCORE, 0);
         editor.apply();
     }
 }
